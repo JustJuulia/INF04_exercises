462724cd36da9fde27303751fa3a6e03441a3e0e
Julia <102248096+JustJuulia@users.noreply.github.com>
2025-01-23 15:46:31 +0100

    added logic for the exercise

--- mobile/inf04_quiz/app/src/main/java/com/example/inf04_quiz/MainActivity.java
+++ mobile/inf04_quiz/app/src/main/java/com/example/inf04_quiz/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,12 +16,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ListView mojalista = findViewById(R.id.pytanie_listview);
+        String[] pytanie = {"Czy Francja istnieje", "Ile to 2+ 10", "Random pytanie"};
+        String[] odp_1 = {"Nie", "2137 jol", "Francja nigdy nie istaniala i nigdy nie bedzie istniec to stan umyslu"};
+        String[] odp_2 = {"Może", "210", "Ta odp powinna byc poprawna"};
+        String[] odp_3 = {"Francja nigdy nie istaniala i nigdy nie bedzie istniec to stan umyslu", "ass", "Odpowiedź 3"};
+        Integer[] poprawna_odp = {1, 2, 3};
+        pytanieModel adapter = new pytanieModel(
+                this,
+                R.layout.layout_pytanie,
+                pytanie,
+                odp_1,
+                odp_2,
+                odp_3,
+                poprawna_odp
+        );
+
+        mojalista.setAdapter(adapter);
     }
-    ListView mojalista = findViewById(R.id.pytanie_listview);
-    
 }
--- mobile/inf04_quiz/app/src/main/java/com/example/inf04_quiz/pytanieModel.java
+++ mobile/inf04_quiz/app/src/main/java/com/example/inf04_quiz/pytanieModel.java
@@ -1,24 +1,25 @@
 package com.example.inf04_quiz;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+public class pytanieModel extends ArrayAdapter<String> {
 
-import java.lang.reflect.Array;
-
-public class pytanieModel {
     String[] pytanie;
-    Context mContext;
     String[] odp_1;
     String[] odp_2;
     String[] odp_3;
-    Integer poprawna_odp;
+    Integer[] poprawna_odp;
 
-    public pytanieModel(String[] pytanie, String[] odp_1, String[] odp_2, String[] odp_3, Integer poprawna_odp) {
-        mContext = mContext;
+    public pytanieModel(Context context, int resource, String[] pytanie, String[] odp_1, String[] odp_2, String[] odp_3, Integer[] poprawna_odp) {
+        super(context, resource, pytanie);
         this.pytanie = pytanie;
         this.odp_1 = odp_1;
         this.odp_2 = odp_2;
@@ -26,31 +27,45 @@ public class pytanieModel {
         this.poprawna_odp = poprawna_odp;
     }
 
-    public String[] getQuestion() {
-        return pytanie;
-    }
-    public String[] getQuestions(){
-        String[] mojepytania = {odp_1, odp_2, odp_3};
-        return mojepytania;
-    }
-    public int correctId(){
-        return poprawna_odp;
-    }
-
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.layout_pytanie, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_pytanie, parent, false);
         }
 
-        TextView titleTextView = (TextView) convertView.findViewById(R.id.text_pytanie);
-        RadioButton odp1 = (RadioButton) convertView.findViewById(R.id.radio_odp1);
-        RadioButton odp2 = (RadioButton) convertView.findViewById(R.id.radio_odp2);
-        RadioButton odp3 = (RadioButton) convertView.findViewById(R.id.radio_odp3);
+        TextView titleTextView = convertView.findViewById(R.id.text_pytanie);
+        RadioButton odp1 = convertView.findViewById(R.id.radio_odp1);
+        RadioButton odp2 = convertView.findViewById(R.id.radio_odp2);
+        RadioButton odp3 = convertView.findViewById(R.id.radio_odp3);
+
         titleTextView.setText(pytanie[position]);
         odp1.setText(odp_1[position]);
         odp2.setText(odp_2[position]);
         odp3.setText(odp_3[position]);
+        LinearLayout pytanie_linear = convertView.findViewById(R.id.pytanie_id);
+        odp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if((poprawna_odp[position] + 1) == 1){
+                    pytanie_linear.setBackgroundColor(Color.parseColor("#097969"));
+                }
+                else{
+                    pytanie_linear.setBackgroundColor(Color.parseColor("#FF5733"));
+                }
+            }
+        });
+        odp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+        odp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return convertView;
     }
 }
