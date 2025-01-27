15b644261300668315391f39a45ecd1301deada2
Julia <102248096+JustJuulia@users.noreply.github.com>
2025-01-27 22:08:57 +0100

    Fixed exercise

--- web/inf04_zimowa_pobrania/project_react/src/App.js
+++ web/inf04_zimowa_pobrania/project_react/src/App.js
@@ -1,109 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [tablica, setTablica] = useState([
-    { id: 0, alt: "Mak", filename: "obraz1.jpg", category: 1, downloads: 35 },
-    { id: 1, alt: "Bukiet", filename: "obraz2.jpg", category: 1, downloads: 43 },
-    { id: 2, alt: "Dalmatyńczyk", filename: "obraz3.jpg", category: 2, downloads: 2 },
-    { id: 3, alt: "Świnka morska", filename: "obraz4.jpg", category: 2, downloads: 53 },
-    { id: 4, alt: "Rotwailer", filename: "obraz5.jpg", category: 2, downloads: 43 },
-    { id: 5, alt: "Audi", filename: "obraz6.jpg", category: 3, downloads: 11 },
-    { id: 6, alt: "kotki", filename: "obraz7.jpg", category: 2, downloads: 22 },
-    { id: 7, alt: "Róża", filename: "obraz8.jpg", category: 1, downloads: 33 },
-    { id: 8, alt: "Świnka morska", filename: "obraz9.jpg", category: 2, downloads: 123 },
-    { id: 9, alt: "Foksterier", filename: "obraz10.jpg", category: 2, downloads: 22 },
-    { id: 10, alt: "Szczeniak", filename: "obraz11.jpg", category: 2, downloads: 12 },
-    { id: 11, alt: "Garbus", filename: "obraz12.jpg", category: 3, downloads: 321 }
-  ]);
-  
-  const [kwiatki, setKwiatki] = useState(true);
-  const [zwierzaczki, setZwierzaczki] = useState(true);
-  const [autka, setAutka] = useState(true);
+const data = [
+  {id: 0, alt: "Mak", filename: "obraz1.jpg", category:1, downloads: 35},
+  {id: 1, alt:"Bukiet", filename: "obraz2.jpg", category: 1, downloads: 43},
+  {id: 2, alt:"Dalmatyńczyk", filename: "obraz3.jpg", category:2, downloads: 2},
+  {id: 3, alt:"Świnka morska", filename: "obraz4.jpg", category:2, downloads: 53},
+  {id: 4, alt:"Rotwailer", filename: "obraz5.jpg", category:2, downloads: 43},
+  {id: 5, alt:"Audi", filename: "obraz6.jpg", category:3, downloads: 11},
+  {id: 6, alt:"kotki", filename: "obraz7.jpg", category:2, downloads: 22},
+  {id: 7, alt:"Róża", filename: "obraz8.jpg", category:1, downloads: 33},
+  {id: 8, alt:"Świnka morska", filename: "obraz9.jpg", category:2, downloads: 123},
+  {id: 9, alt:"Foksterier", filename: "obraz10.jpg", category:2, downloads: 22},
+  {id: 10, alt:"Szczeniak", filename: "obraz11.jpg", category:2, downloads: 12},
+  {id: 11, alt:"Garbus", filename: "obraz12.jpg", category:3, downloads: 321}
+];
 
-  const kwiatkiChange = () => {
-    setKwiatki(prev => !prev);
-  };
+const Tile = ({imgSrc, downloads, imgAlt}) => {
+  const [count, setCout] = useState(downloads);
 
-  const autaChange = () => {
-    setAutka(prev => !prev);
-  };
-
-  const zwierzakiChange = () => {
-    setZwierzaczki(prev => !prev);
-  };
-  const incrementDownloads = (id) => {
-    setTablica(prevTablica => prevTablica.map(tab => 
-      tab.id === id ? { ...tab, downloads: tab.downloads + 1 } : tab
-    ));
-  };
+  const handle_button = () => {
+    setCout( count + 1 );
+  }
 
   return (
-    <>
-      <h3>Kategorie zdjęć</h3>
-      <div className='d-flex'>
-        <div class="form-check form-switch">
-          <input
-            class="form-check-input kwiaty"
-            type="checkbox"
-            role="switch"
-            id="kwiaty"
-            checked={kwiatki}
-            onChange={kwiatkiChange}
-          />
-          <label class="form-check-label" htmlFor="kwiaty">Kwiaty</label>
+    <div>
+      <img src={imgSrc} alt={imgAlt} />
+      <div>
+        <div>
+        Pobrań: {count}
         </div>
-        <div class="form-check form-switch">
-          <input
-            class="form-check-input zwierzeta"
-            type="checkbox"
-            role="switch"
-            id="zwierzeta"
-            checked={zwierzaczki}
-            onChange={zwierzakiChange}
-          />
-          <label class="form-check-label" htmlFor="zwierzeta">Zwierzęta</label>
-        </div>
-        <div class="form-check form-switch">
-          <input
-            class="form-check-input samochody"
-            type="checkbox"
-            role="switch"
-            id="samochody"
-            checked={autka}
-            onChange={autaChange}
-          />
-          <label class="form-check-label" htmlFor="samochody">Samochody</label>
-        </div>
+        <input className={"btn btn-success"} value={"Pobierz"} type='button' onClick={handle_button} />
       </div>
-      <div className='d-flex flex-wrap gap-5 wszystko'>
-        {tablica.map((tab) => (
-          <div key={tab.id} className='zdjecia'>
-            <div>
-              {((tab.category === 1 && kwiatki) || 
-                (tab.category === 2 && zwierzaczki) || 
-                (tab.category === 3 && autka)) && (
-                <div>
-                  <img src={`./${tab.filename}`} alt={tab.alt} />
-                  <p>Liczba pobran: {tab.downloads}</p>
-                  <div className='btn btn-success'>
-                    <a 
-                      href={`./${tab.filename}`} 
-                      download 
-                      className='text-white'
-                      onClick={() => incrementDownloads(tab.id)}
-                    >
-                      Pobierz
-                    </a>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
-        ))}
+    </div>
+  );
+}
+
+function App() {
+
+  const [kwiaty, setKwiaty] = useState(true);
+  const [zwierzeta, setZwierzeta] = useState(true);
+  const [samochody, setSamochody] = useState(true);
+
+  return (
+    <div>
+      <div>
+        Kategorie zdjęć
       </div>
-    </>
+      <div>
+        <input className='form-check-input' type='checkbox' defaultChecked={kwiaty} onChange={ () => { setKwiaty(!kwiaty) } } /> Kwiaty
+        <input className='form-check-input' type='checkbox' defaultChecked={zwierzeta} onChange={ () => { setZwierzeta(!zwierzeta) } } /> Zwierzeta 
+        <input className='form-check-input' type='checkbox' defaultChecked={samochody} onChange={ () => { setSamochody(!samochody) } } /> Samochody 
+      </div>
+
+      {
+        data.map(( element, index ) => {
+          if (!kwiaty && element.category === 1) { return null; }
+          if (!zwierzeta && element.category === 2) { return null; }
+          if (!samochody && element.category === 3) { return null; }
+          
+          const filename = `assets/` + element.filename;
+          return (
+            <Tile key={element.id} imgAlt={element.alt} imgSrc={filename} downloads={element.downloads} />
+          )
+        })
+      }
+    </div>
   );
 }
 
